6923d67b02710e7daf98b796cdd43cfca662bfb8
Tunacodin <bostancibasituna58@gmail.com>
2024-05-10 14:57:38 +0300

    web

--- Web/web/src/App.js
+++ Web/web/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link, NavLink, useNavigate } from "react-router-dom";
+import Admin from "./pages/Admin";
+import { LoginPage } from "./pages/LoginPage";
 
-function App() {
+export default function App() {
+  const navigate = useNavigate();
+
+  // Giriş yapıldıktan sonra yönlendirme fonksiyonu
+  const handleLoginSuccess = () => {
+    navigate("/Admin");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<LoginPage onLoginSuccess={handleLoginSuccess} />}
+        />
+        <Route path="/Admin" element={<Admin />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
