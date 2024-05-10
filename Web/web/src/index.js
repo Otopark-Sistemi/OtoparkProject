6426ab62cd47f961f4e9b94733ecfa00fc834532
emreolmez26 <emreolmez2626@gmail.com>
2024-05-10 16:44:28 +0300

    first commit

---
+++ Web/web/src/index.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
+);
