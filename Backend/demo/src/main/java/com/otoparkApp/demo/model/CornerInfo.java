c9e0d36abc452fad009d4e44096fa3242e2e071e
Halil <halilattar01@gmail.com>
2024-05-10 15:31:05 +0300

    user model added

--- Backend/demo/src/main/java/com/otoparkApp/demo/model/CornerInfo.java
+++ Backend/demo/src/main/java/com/otoparkApp/demo/model/CornerInfo.java
@@ -1,11 +1,13 @@
 package com.otoparkApp.demo.model;
 
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.util.List;
 
 @Entity
@@ -15,9 +17,12 @@ public class CornerInfo {
 
 
     @Id
-    private long id;
+    private Long id;
 
     private String block_name;
+
+    @ElementCollection
+    private List<String> corner_number_list;
 
 
 }
